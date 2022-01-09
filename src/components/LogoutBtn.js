7d6488120b99c1350b11cd00171b0c5114172ca5
yaserananbeh <yaser.ananbeh74@gmail.com>
2022-01-09 14:08:23 +0200

    empty the cart after logout

--- src/components/LogoutBtn.js
+++ src/components/LogoutBtn.js
@@ -1,13 +1,19 @@
 import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { IsAuthContext } from "../App";
+import { CartCounterContext } from "../App";
 
 function LogoutBtn() {
+  const cartCounterSetter = useContext(CartCounterContext);
+
   const { setIsAuth } = useContext(IsAuthContext);
   const navigate = useNavigate();
 
   const handleLogoutClick = () => {
     setIsAuth({ email: "guest" });
+    localStorage.removeItem("cartItems");
+    cartCounterSetter(0);
+
     navigate("/");
   };
   return (
